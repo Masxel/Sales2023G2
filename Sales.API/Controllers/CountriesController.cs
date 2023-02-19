15036f6aa4f460a9f6e2a6b357cfa4589b553ebf
Masxel <44252859+Masxel@users.noreply.github.com>
2023-02-18 22:08:40 -0500

    Primeros controlladores

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public CountriesController(DataContext context)         
+        {
+            _dataContext = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _dataContext.Countries.ToListAsync());
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            try
+            {
+                _dataContext.Countries.Add(country);
+                await _dataContext.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (Exception ex)
+            {
+                return NoContent();
+            }
+        }
+        
+
+
+    }
+}
--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
@@ -15,6 +19,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 
 app.UseHttpsRedirection();
 
