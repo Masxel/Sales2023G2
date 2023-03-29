8d73c6387ec2bfd195b6fb8247c70d5a78aa2e65
Masxel <44252859+Masxel@users.noreply.github.com>
2023-03-28 22:47:23 -0500

    Edit user is done

--- Sales.API/Helpers/UserHelper.cs
+++ Sales.API/Helpers/UserHelper.cs
@@ -8,57 +8,73 @@ namespace Sales.API.Helpers
 {
     public class UserHelper : IUserHelper
     {
-            private readonly DataContext _context;
-            private readonly UserManager<User> _userManager;
-            private readonly RoleManager<IdentityRole> _roleManager;
-            private readonly SignInManager<User> _signInManager;
+        private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
 
-            public UserHelper(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
-                SignInManager<User> signInManager)
-            {
-                      _context = context;
-                  _userManager = userManager;
-                  _roleManager = roleManager;
-                _signInManager = signInManager;
-            }
+        public UserHelper(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
+            SignInManager<User> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
 
-            public async Task<IdentityResult> AddUserAsync(User user, string password)
-            {
-                return await _userManager.CreateAsync(user, password);
-            }
+        public async Task<IdentityResult> AddUserAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
 
-            public async Task AddUserToRoleAsync(User user, string roleName)
-            {
-                await _userManager.AddToRoleAsync(user, roleName);
-            }
+        public async Task AddUserToRoleAsync(User user, string roleName)
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
 
-            public async Task CheckRoleAsync(string roleName)
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        }
+
+        public async Task CheckRoleAsync(string roleName)
+        {
+            bool roleExists = await _roleManager.RoleExistsAsync(roleName);
+            if (!roleExists)
             {
-                bool roleExists = await _roleManager.RoleExistsAsync(roleName);
-                if (!roleExists)
+                await _roleManager.CreateAsync(new IdentityRole
                 {
-                    await _roleManager.CreateAsync(new IdentityRole
-                    {
-                        Name = roleName
-                    });
-                }
+                    Name = roleName
+                });
             }
+        }
 
-            public async Task<User> GetUserAsync(string email)
-            {
-                var user = await _context.Users
-                    .Include(u => u.City!)
-                    .ThenInclude(c => c.State!)
-                    .ThenInclude(s => s.Country!)
-                    .FirstOrDefaultAsync(x => x.Email == email);
-
+        public async Task<User> GetUserAsync(string email)
+        {
+            var user = await _context.Users
+                .Include(u => u.City!)
+                .ThenInclude(c => c.State!)
+                .ThenInclude(s => s.Country!)
+                .FirstOrDefaultAsync(x => x.Email == email);
             return user!;
-            }
 
-            public async Task<bool> IsUserInRoleAsync(User user, string roleName)
-            {
-                return await _userManager.IsInRoleAsync(user, roleName);
-            }
+        }
+
+        public async Task<User> GetUserAsync(Guid userId)
+        {
+            var user = await _context.Users
+            .Include(u => u.City!)
+            .ThenInclude(c => c.State!)
+            .ThenInclude(s => s.Country!)
+            .FirstOrDefaultAsync(x => x.Id == userId.ToString());
+            return user!;
+
+        }
+
+        public async Task<bool> IsUserInRoleAsync(User user, string roleName)
+        {
+            return await _userManager.IsInRoleAsync(user, roleName);
+        }
 
         public async Task<SignInResult> LoginAsync(LoginDTO model)
         {
@@ -69,6 +85,11 @@ namespace Sales.API.Helpers
         {
             await _signInManager.SignOutAsync();
         }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
     }
 
-    }
+}
