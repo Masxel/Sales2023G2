4dea83ed018d374c759f1f57e7558ce2629467da
Masxel <44252859+Masxel@users.noreply.github.com>
2023-04-09 18:14:06 -0500

    show products car

--- Sales.Shared/DTOs/TemporalSaleDTO.cs
+++ Sales.Shared/DTOs/TemporalSaleDTO.cs
@@ -8,6 +8,9 @@ namespace Sales.Shared.DTOs
 {
     public class TemporalSaleDTO
     {
+
+        public int Id { get; set; }
+
         public int ProductId { get; set; }
 
         public float Quantity { get; set; } = 1;
